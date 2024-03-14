5d24d7a8c7162bd7e80e6f3cfb045c393937500a
Chad Smith <smithchad17@gmail.com>
2024-03-14 15:28:53 -0400

    -Added repositories and respective interfaces -Registered the repository service in Program.cs

---
+++ APITestBed/Repositories/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using APITestBed.Data;
+using APITestBed.Interfaces;
+using APITestBed.Models;
+
+namespace APITestBed.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly DataContext _context;
+
+        public BookRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<BookModel> GetBooks()
+        {
+            return _context.Books.OrderBy(b => b.Book_ID).ToList();
+        }
+    }
+}
